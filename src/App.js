30dc89c5b275623fe3bcb83fc0cb4582472a4c41
Mark Staebler <markstaebler@gmail.com>
2018-01-20 12:46:59 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Jumbotron } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,21 +7,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <p className="App-intro">
-          <h2>Sign up for updates on the next great board game</h2>
-          <h1>Contagion</h1>
-          <form action="mailto:mail@tablebossgames.com" method="post" enctype="text/plain">
-            Name:
-            <input type="text" name="name"></input><br />
-            Email:
-            <input type="text" name="mail"></input><br />
-            <input type="submit" value="Send"></input>
-            <input type="reset" value="Reset"></input>
-          </form>
-        </p>
+        <Jumbotron>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </Jumbotron>
+        <Grid>
+          <p className="App-intro">
+            <h2>Sign up for updates on the next great board game</h2>
+            <h1 className="Game-title">Contagion</h1>
+            <form action="mailto:mail@tablebossgames.com" method="post" enctype="text/plain">
+              <input type="text" name="name" placeholder="Name"></input><br />
+              <input type="text" name="mail" placeholder="Email"></input><br />
+              <input type="submit" value="Send"></input>
+              <input type="reset" value="Reset"></input>
+            </form>
+          </p>
+        </Grid>
       </div>
     );
   }
